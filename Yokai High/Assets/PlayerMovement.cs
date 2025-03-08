c110b49f7d0b23ccd1f398c6934de43ca314b3e5
David Jávorcsík <132663385+RedLobo1@users.noreply.github.com>
2025-03-08 15:40:46 +0100

    Models added

--- Yokai High/Assets/PlayerMovement.cs
+++ Yokai High/Assets/PlayerMovement.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Rigidbody rb;
-    [SerializeField] float speed = 1f;
+    private Rigidbody rb;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Transform cameraTransform;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.AddForce(Vector3.right * Input.GetAxis("Horizontal") * speed, ForceMode.VelocityChange);
-        rb.AddForce(Vector3.forward * Input.GetAxis("Vertical") * speed, ForceMode.VelocityChange);
+        MovePlayer();
+    }
 
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
 
+        // Get the camera's forward and right directions
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        // Flatten the directions to prevent unwanted vertical movement
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        // Calculate movement direction relative to the camera
+        Vector3 moveDirection = (right * moveX + forward * moveZ).normalized;
+
+        // Stop movement when no input is given
+        if (moveDirection.magnitude == 0)
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector3(moveDirection.x * speed, rb.velocity.y, moveDirection.z * speed);
+        }
     }
 }
